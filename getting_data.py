d7636f70b621d6667d5bc1be7f6fc4d2114312f9
Vincenzo DiMatteo <47278634+Vman11@users.noreply.github.com>
2024-09-19 13:45:55 -0400

    can be multithreaded

--- getting_data.py
+++ getting_data.py
@@ -8,12 +8,30 @@ import json
 import requests
 import scriptconfig as scfg
 import ubelt as ub
-
+import kwutil
+import os.path
 
 class getting_dataCLI(scfg.DataConfig):
     file_path = scfg.Value('/home/vinnie/code/aria_playground/Nymeria_download_urls.json', help = "Provide a json file to read data")
 
     out_fpath = scfg.Value('/home/vinnie/Desktop/nymera_res', help = "Provide location for data outputted")
+
+    workers = scfg.Value(4 ,  help="Describe number of workers for downloading files")
+    
+    num_sequences = scfg.Value(0, help="Get number of sequences based on number described if 0 is picked then it will download all sequences")
+
+def get_sequence_assest(filename, download_url):
+    if not os.path.isfile(filename):
+        response = requests.get(download_url)
+        if response.status_code == 200:  # Check if the request was successful
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+        else:
+            print("No file downloaded")
+    else:
+        print('already downloaded moving on')
+
+
 
 
 def main():
@@ -43,20 +61,28 @@ def main():
 
 
     #The following items are avalable in this
-    count = 0 
-    for k in data['sequences'].keys():
-        sequence = data['sequences'][k]
-        dpath_seq = (dpath / k).ensuredir()
-        if count == 0 :
-            for k_2 in sequence.keys():
-                filename =  dpath_seq /  sequence[k_2]['filename']
-                response = requests.get(sequence[k_2]['download_url'])
-                if response.status_code == 200:  # Check if the request was successful
-                    with open(filename, 'wb') as file:
-                        file.write(response.content)
-                else:
-                    print("No file downloaded")
-        count+=1
+    num_sequences = config['num_sequences'] 
+    count = 0
+    pman = kwutil.ProgressManager()
+    pool  = ub.JobPool('thread', max_workers=config['workers'])
+
+
+    with pman:
+        for k in data['sequences'].keys():
+            sequence = data['sequences'][k]
+            dpath_seq = (dpath / k).ensuredir()
+            count_k2=0
+            if count <= num_sequences :
+                for k_2 in sequence.keys():
+                    filename =  dpath_seq /  sequence[k_2]['filename']
+                    download_url = sequence[k_2]['download_url']
+                    pool.submit(get_sequence_assest,filename,download_url)
+                    count_k2+=1
+                job_prog = pman.progiter(pool.as_completed(desc='downloading...'),total=count_k2)
+                for job in job_prog:
+                    ...
+                
+            count+=1
 
     #print(sequence.keys())
 
